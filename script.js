2c8829bbc3bb60f43daa873ac2063e115524a2be
Ankit Sharma <ankitzsharma252004@gmail.com>
2024-05-04 17:54:19 +0530

    My Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
